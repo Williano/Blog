3fd068acffe50712cd34c81d2590bf6edb1c5e2a
Williano <paawilly17@gmial.com>
2018-02-17 11:45:43 -1200

    Added Allowed Host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%_#ni&+)!ggscp8hgvioxhg5vak+r6^r1_l8yln83x$wpig3*u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
